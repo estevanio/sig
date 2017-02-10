f1ee0f501bdade6c52f0bef1119feac17abe0da4
stavinopolis <estevanbenjamin@gmail.com>
2017-02-09 21:44:56 -0800

    got the directive working and the dialog now time to combine yaoooo

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
-.controller('AppCtrl', function($scope, $mdDialog) {
-  $scope.status = '  ';
-  $scope.customFullscreen = false;
-
+.controller('AppCtrl', function($scope, $mdDialog, $sigapi) {
+  // $scope.status = '  ';
+  // $scope.customFullscreen = false;
 
 
 
@@ -26,9 +25,6 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
 
 
-})
-
-.directive('sig', [function() {
 
   function DialogController($scope, $mdDialog) {
     $scope.hide = function() {
@@ -44,41 +40,115 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
     };
   }
 
-  function Controller($scope, $mdDialog) {
-    $scope.status = '  ';
-    $scope.customFullscreen = false;
 
 
+})
+
+.directive('sigdir', ['$timeout', '$q',
+  function($timeout, $q) {
+    return {
+      restrict: 'A',
+      link: function($scope, $element, $attr) {
+        function initialize() {
+          console.log("initialized");
+          var wrapper = $element[0],
+            clearButton = wrapper.querySelector("[data-action=clear]"),
+            saveButton = wrapper.querySelector("[data-action=save]"),
+            canvas = wrapper.querySelector("canvas"),
+            signaturePad;
+
+            signaturePad = new SignaturePad(canvas);
+            console.log($element[0]);
+
+          function resizeCanvas() {
+            var ratio = Math.max(window.devicePixelRatio || 1, 1);
+            canvas.width = canvas.offsetWidth * ratio;
+            canvas.height = canvas.offsetHeight * ratio;
+            canvas.getContext("2d").scale(ratio, ratio);
+          }
+
+          window.onresize = resizeCanvas;
+          resizeCanvas();
 
 
-    $scope.showdialog = function(ev) {
-      $mdDialog.show({
-          controller: DialogController,
-          templateUrl: 'dialog1.tmpl.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose: false,
-          fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-        })
-        .then(function(answer) {
-          $scope.status = 'You said the information was "' + answer + '".';
-        }, function() {
-          $scope.status = 'You cancelled the dialog.';
-        });
+          clearButton.addEventListener("click", function(event) {
+            signaturePad.clear();
+          });
+
+          saveButton.addEventListener("click", function(event) {
+            if (signaturePad.isEmpty()) {
+              alert("Please provide signature first.");
+            } else {
+              console.log(signaturePad.toDataURL());
+              signaturePad.clear();
+            }
+          });
+
+
+        }
+        if (document.readyState === 'complete') {
+          $timeout(initialize, 0, false);
+        } else {
+          console.log("readyState Incomplete");
+
+
+          document.onreadystatechange = function() {
+            if (document.readyState === "complete") {
+              console.log("dehh");
+
+              $timeout(initialize, 0, false);
+            }
+          };
+          // $helmPlatform.ready(function () { google.maps.event.addDomListener(window, 'load', initialize); });
+        }
+      }
     };
-
   }
-  return {
-    restrict: 'A',
-    link: function(scope, iElement, iAttrs) {
+])
 
-    },
-    controller: Controller
+
+.service('$sigapi', ['$http', function($http) {
+
+
+  return {
+    open: open
   };
 }]);
 
 
-/**
-Copyright 2016 Google Inc. All Rights Reserved. 
-Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
-**/
+// var wrapper = document.getElementById("signature-pad"),
+//     clearButton = wrapper.querySelector("[data-action=clear]"),
+//     saveButton = wrapper.querySelector("[data-action=save]"),
+//     canvas = wrapper.querySelector("canvas"),
+//     signaturePad;
+
+// // Adjust canvas coordinate space taking into account pixel ratio,
+// // to make it look crisp on mobile devices.
+// // This also causes canvas to be cleared.
+// function resizeCanvas() {
+//     // When zoomed out to less than 100%, for some very strange reason,
+//     // some browsers report devicePixelRatio as less than 1
+//     // and only part of the canvas is cleared then.
+//     var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+//     canvas.width = canvas.offsetWidth * ratio;
+//     canvas.height = canvas.offsetHeight * ratio;
+//     canvas.getContext("2d").scale(ratio, ratio);
+// }
+
+// window.onresize = resizeCanvas;
+// resizeCanvas();
+
+// signaturePad = new SignaturePad(canvas);
+
+// clearButton.addEventListener("click", function (event) {
+//     signaturePad.clear();
+// });
+
+// saveButton.addEventListener("click", function (event) {
+//     if (signaturePad.isEmpty()) {
+//         alert("Please provide signature first.");
+//     } else {
+//        console.log(signaturePad.toDataURL());
+//         signaturePad.clear();
+//     }
+// });
